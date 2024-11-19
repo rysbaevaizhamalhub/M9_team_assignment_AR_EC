d9d2f84726204c976807320eafd97ad91123521b
rysbaevaizhamalhub <rysbaevaizhamal@gmail.com>
2024-11-19 15:12:37 -0600

    Update some_python_program.py

--- some_python_program.py
+++ some_python_program.py
@@ -5,20 +5,21 @@
 # Updated by:
 #
 #
-# Document what the following lines of code do here
+# 1 Document what the following lines of code do here
 from tkinter import *
-
+# this line imports all classes, functions from tkinter module, which gives access to tools to build GUI
 root = Tk()
-
+# creates the main window for application
 root.title("Simple Calculator")
+# sets the title of main window to "Simple Calculator"
 
-# Document what the following lines of code do here
+# 2 Document what the following lines of code do here
 e = Entry(root, width=35, borderwidth=5)
 # The line e = Entry(root, width=35, borderwidth=5) creates an Entry widget of width 35 characters and border width 5 pixels, placing it inside the parent widget root.
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 # The line e.grid(row=0, column=0, columnspan=3, padx=10, pady=10) places the Entry widget e in the grid at the position of the 0th row and 0th column, spans it across 3 columns, and adds padding on all sides of 10 pixels.
 
-# Document what the following lines of code do here
+# 3 Document what the following lines of code do here
 def button_click(number):
     current = e.get()
     e.delete(0, END)
